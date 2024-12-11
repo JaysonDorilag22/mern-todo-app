05b819faad260099612dd072ad430920667b3d65
JD <jaysondorilag1@gmail.com>
2024-12-12 00:25:44 +0800

    Add authentication and user components with path aliases

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
+import SignIn from '@auth/SignIn';
+import SignUp from '@auth/SignUp';
+import Home from '@user/Home';
+import Landing from '@user/Landing';
 import { ModeToggle } from './components/mode-toggle';
 import { ThemeProvider } from './components/theme-provider';
-import { Button } from './components/ui/button'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Button } from './components/ui/button';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-
-
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle/>
-      <Button>Hello</Button>
-    <Router>
-      <Routes>
-        {/* <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/about" element={<h1>About</h1>} /> */}
-
-      </Routes>
-    </Router>
+      <ModeToggle />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
